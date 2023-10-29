131b72da9412aeef185f0540b1446c4fdb0c0d85
vidaibao <vutrungtin@gmail.com>
2023-10-29 20:48:25 +0900

    first commit

---
+++ part1/app.js
@@ -0,0 +1,40 @@
+// DOM Manipulation
+
+
+
+
+
+// Get ElementById
+
+const title = document.getElementById("main-heading");
+console.log(title);
+
+
+
+
+
+// GetElementByClassName
+
+const listItem = document.getElementsByClassName("list-items")
+console.log(listItem);
+
+// Get Element By Tag Name
+
+const tagItem = document.getElementsByTagName("li")
+console.log(tagItem);
+
+
+// query Selector
+
+const container = document.querySelector("div")
+
+console.log(container);
+
+
+// query Selector All
+
+const containerA = document.querySelectorAll("div")
+
+console.log(containerA);
+
+
